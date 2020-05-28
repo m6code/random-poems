2749fa01bb0eb5bfb461761459d69f0b6ddd8ae4
M6code <benjamynfrank101@gmail.com>
2020-05-28 10:16:24 +0100

    Move skeleton logic to Poem component

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -4,7 +4,12 @@ import PropTypes from 'prop-types';
 function Poem(props) {
     return (
         <div>
-            {
+            {props.loading ?
+                <div>
+                    <h3 className="bp3-heading bp3-skeleton">Peom Title  liver of the calf of the calf  it is a huge calf's liver of the calf </h3>
+                    <div className="bp3-skeleton poem">it is a huge calf's liver of the calf we slaughtered yesterday assures me the butcher of my favorite butcher shop but I didn't go to the butcher shop and neither do i have a favorite butcher shop it is my tongue that has fallen out of my mouth this huge liver i'm putting it back pushing it into my throat i give up when i realize that it is choking me my tongue falls out again hangs onto me creeps up my neck licks my body through my tongue through my tongue through my most grievous tongue it is a huge calf's liver of the calf we slaughtered yesterday assures me the butcher of my favorite butcher shopit is a huge</div>
+                    <div className='bp3-skeleton bp3-text-muted author'>Author's Name</div>
+                </div> :
                 props.poems.map((poem, index) => {
                     return (
                         <div key={index} >
@@ -21,6 +26,7 @@ function Poem(props) {
 
 Poem.propTypes = {
     poems: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
 }
 
 export default Poem
--- src/components/Poemlist.js
+++ src/components/Poemlist.js
@@ -6,17 +6,9 @@ import Poem from './Poem';
 
 function Poemlist(props) {
     return (
-        <div>
-            {/* Renders a List of Peom  using the props received from App component  */}
-            {props.loading ?
-                <div>
-                <h3 className="bp3-heading bp3-skeleton">Peom Title</h3>
-                            <div className="bp3-skeleton poem">Some random long poem content goes here saying blah blah balh balh my tongue falls out of my mouth it is no longer a tongue, it is a huge calf's liver of the calf we slaughtered yesterday assures me the butcher of my favorite butcher shop but I didn't go to the butcher shop and neither do i have a favorite butcher shop it is my tongue that has fallen out of my mouth this huge liver i'm putting it back pushing it into my throat i give up when i realize that it is choking me my tongue falls out again hangs onto me creeps up my neck licks my body through my tongue through my tongue through my most grievous tongue</div>
-                            <div className='bp3-skeleton bp3-text-muted author'>Author's Name</div>
-                </div> :
-                <Poem poems={props.poems}/>
-            }
-        </div>
+        <Poem
+            poems={props.poems}
+            loading={props.loading} />
     )
 }
 
